dd00f2161e2c34ae732b5d337f0d0cb7b623ccb6
Octowl <alsaff1987@gmail.com>
2024-11-03 04:56:56 +0300

    write task

---
+++ BMI.js
@@ -0,0 +1,54 @@
+/**
+ * Calculate BMI
+ * - takes a weight in KG
+ * - takes a height in cm
+ * - returns the calculated BMI
+ */
+function calculateBMI(weight, height) {
+  /**
+   *  Write your code here 👇🏼
+   */
+  const BMI = 0;
+
+  /**
+   * Stop making changes here!
+   */
+  return BMI;
+}
+
+/**
+ * Get BMI State
+ * - takes the BMI
+ * - returns a string with the BMI state
+ */
+function getBMIState(BMI) {
+  /**
+   *  Write your code here 👇🏼
+   */
+  let BMIState;
+
+  /**
+   * Stop making changes here!
+   */
+  return BMIState;
+}
+
+/**
+ * Get BMI State by Age
+ * - takes the BMI
+ * - takes the age
+ * - returns a string with the BMI state
+ */
+function getBMIStateByAge(BMI, age) {
+  /**
+   *  Write your code here 👇🏼
+   */
+  let BMIState = "not healthy";
+
+  /**
+   * Stop making changes here!
+   */
+  return BMIState;
+}
+
+module.exports = { calculateBMI, getBMIState, getBMIStateByAge };
