0b2be186479745ad95ffd81848afbdda29649cd6
TariqBazadough <tariq.bazadough@gmail.com>
2025-02-26 19:10:23 +0300

    I have changed the following: 1- changed challenge description 2- added types to the functions 3- edited the test file logic for the 2nd challenge

--- BMI.ts
+++ BMI.ts
@@ -1,53 +1,51 @@
 /**
  * Calculate BMI
- * - takes a weight in KG
- * - takes a height in cm
- * - returns the calculated BMI
+ * - Accepts weight in KG as a parameter (type number)
+ * - Accepts height in CM as a parameter (type number)
+ * - returns the calculated BMI (type number)
+ * - The equation for BMI is (weight (kg) / height^2(m)).
  */
-function calculateBMI(weight, height) {
-  /**
-   *  Write your code here 👇🏼
-   */
+function calculateBMI(weight: number, height: number): number {
+  // Write your code here 👇🏼
   const BMI = 0;
-
-  /**
-   * Stop making changes here!
-   */
+  // Stop making changes here!
   return BMI;
 }
 
 /**
  * Get BMI State
- * - takes the BMI
- * - returns a string with the BMI state
+ * - Accepts BMI as a parameter (type number)
+ * - returns a BMIStatus type indicating the BMI category (type BMIStatus)
  */
-function getBMIState(BMI) {
-  /**
-   *  Write your code here 👇🏼
-   */
-  let BMIState;
 
-  /**
-   * Stop making changes here!
-   */
+// Don't change this type
+type BMIStatus = "underweight" | "healthy" | "overweight" | "obese";
+
+function getBMIState(BMI: number): BMIStatus {
+  // Don't change this variable
+  let BMIState: BMIStatus = "obese";
+  // Write your code here 👇🏼
+
+  // Stop making changes here!
   return BMIState;
 }
 
 /**
  * Get BMI State by Age
- * - takes the BMI
- * - takes the age
- * - returns a string with the BMI state
+ * - Accepts BMI as a parameter (type number)
+ * - Accepts age as a parameter (type number)
+ * - returns a HealthStatus type indicating the BMI state (type HealthStatus)
  */
-function getBMIStateByAge(BMI, age) {
-  /**
-   *  Write your code here 👇🏼
-   */
-  let BMIState = "not healthy";
 
-  /**
-   * Stop making changes here!
-   */
+// Don't change this type
+type HealthStatus = "not healthy" | "healthy";
+
+function getBMIStateByAge(BMI: number, age: number): HealthStatus {
+  // Don't change this variable
+  let BMIState: HealthStatus = "not healthy";
+  // Write your code here 👇🏼
+
+  // Stop making changes here!
   return BMIState;
 }
 
